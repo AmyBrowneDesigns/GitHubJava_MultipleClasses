a1fcbf3b3e9c9eb6e77d4194faa02a02a1234e14
AmyBrowneDesigns <amybrowne20@gmail.com>
2020-11-20 17:39:05 +0000

    added tests for commit description and unique id

--- src/main/java/Commit.java
+++ src/main/java/Commit.java
@@ -1,2 +1,22 @@
 public class Commit {
+
+    private String description;
+    private int uniqueID;
+
+    public Commit ( String description, int UniqueID){
+        this.description =description;
+        this.uniqueID = uniqueID;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description) {//test this
+        this.description = description;
+    }
+
+    public int getUniqueID(){
+        return this.uniqueID;
+    }
 }
--- src/test/java/CommitTest.java
+++ src/test/java/CommitTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CommitTest {
+
+    Commit commit;
+
+    @Before
+    public void before(){
+        commit = new Commit ("Added Components", 20091987);
+    }
+
+    @Test
+    public void hasDescription(){
+        assertEquals("Added Components", commit.getDescription());
+    }
+
+    @Test
+    public void hasUniqueId(){
+        assertEquals(0, commit.getUniqueID());
+    }
 }
